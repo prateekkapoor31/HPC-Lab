328e2933b52e2a1fd3615b92a93ace4fd2f8ed11
prateekkapoor31 <prateekkapoor31@gmail.com>
2017-11-16 01:11:17 +0530

    Update Largest_Number_Locks.c

--- OPENMP/Largest_Number_Locks.c
+++ OPENMP/Largest_Number_Locks.c
@@ -1,85 +1,38 @@
-//7. Write an OpenMP program to find the largest element in an array using locks. 
+//Write an OpenMP program to find the largest element in an array using locks.
+
 #include <stdio.h>
 #include <omp.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-#define MINUS_INFINITY -9999
-#define MAXIMUM_VALUE 65535
+#define N 1000
+#define NUM_THREADS 4
 
-/* Main Program */
 
-main()
+int main()
 {
-int *array, i, Noofelements, cur_max, current_value;
-omp_lock_t MAXLOCK;
+	int arr[N],i;
+	int max = -1;
 
-printf("Enter the number of elements\n");
-scanf("%d", &Noofelements);
+	omp_lock_t lock;
+	omp_init_lock(&lock);
+	
+	omp_set_num_threads(NUM_THREADS);
 
-if (Noofelements <= 0) {
-printf("The array elements cannot be stored\n");
-exit(1);
-}
-/* Dynamic Memory Allocation */
-array = (int *) malloc(sizeof(int) * Noofelements);
+	for(i=0;i<N;i++)
+		arr[i] = rand()%100000;
 
-/* Allocating Random Number To Array Elements */
 
-srand(MAXIMUM_VALUE);
-for (i = 0; i < Noofelements; i++)
-array[i] = rand();
+	#pragma omp parallel for
+	for(i=0;i<N;i++)
+	{
+		if(arr[i]>max)
+		{
+			omp_set_lock(&lock);
+			if(arr[i]>max)
+				max = arr[i];
+			omp_unset_lock(&lock);
+		}
+	}
 
-if (Noofelements == 1) {
-printf("The Largest Element In The Array Is %d", array[0]);
-exit(1);
-}
-/* Initializing The Lock */
-
-printf("The locking is going to start\n");
-
-omp_set_num_threads(8);
-omp_init_lock(&MAXLOCK);
-cur_max = MINUS_INFINITY;
-printf("the lock s initialized\n");
-/* OpenMP Parallel For Directive And Lock Functions */
-
-#pragma omp parallel for
-for (i = 0; i < Noofelements; i = i + 1) {
-if (array[i] > cur_max) {
-omp_set_lock(&MAXLOCK);
-if (array[i] > cur_max)
-cur_max = array[i];
-omp_unset_lock(&MAXLOCK);
-}
-}
-
-/* Destroying The Lock */
-
-omp_destroy_lock(&MAXLOCK);
-
-/* Serial Calculation */
-current_value = array[0];
-for (i = 1; i < Noofelements; i++)
-if (array[i] > current_value)
-current_value = array[i];
-
-printf("The Array Elements Are \n");
-
-for (i = 0; i < Noofelements; i++)
-printf("\t%d", array[i]);
-
-/* Checking For Output Validity */
-
-if (current_value == cur_max)
-printf("\nThe Max Value Is Same For Serial And Using Parallel OpenMP Directive\n");
-else {
-printf("\nThe Max Value Is Not Same In Serial And Using Parallel OpenMP Directive\n");
-exit(1);
-}
-
-/* Freeing Allocated Memory */
-
-free(array);
-
-printf("\nThe Largest Number Of The Array Is %d\n", cur_max);
+	printf("Max = %d\n",max);
 }
